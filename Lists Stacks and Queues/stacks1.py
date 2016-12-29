80e9a048b92ab83f460e0fca1d6302a0f4f8b20c
Ashutosh Gajankush <agajanku@stevens.edu>
2016-12-29 18:20:01 -0500

    Stacks Implementation

--- Lists Stacks and Queues/stacks1.py
+++ Lists Stacks and Queues/stacks1.py
@@ -3,22 +3,31 @@ Stacks as Lists.
 An implementation of opening and closing round braces checker, in string
 
 """
+
 def parChecker(string):
 	alist = []
 	balanced = True
 	for i in range(len(string)):
-		if(string[i] == "("):
+		if(string[i] in "([{"):
 			alist.append(string[i])
 		else:
 			if(len(string) == 0):
 				balanced = False
 			else:
-				alist.pop()
+				value = alist.pop()
+				if not matches(value, string[i]):
+					balanced = False
+
 	if(balanced and len(alist) == 0):
 		return True
 	else:
 		return False
 
-print parChecker('((()))')
-print parChecker('((()')
+def matches(open, close):
+	opens = "([{"
+	closers = ")]}"
+	return opens.index(open) == closers.index(close)
+
+print parChecker('{{([][])}()}')
+print parChecker('([)]')
 
